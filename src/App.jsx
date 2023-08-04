d8541732a7706f1599c183a05419dde13f13e0c5
muwaahib <muhaabdi71@gmail.com>
2023-08-04 08:53:16 +0300

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/header/Header'
-import About from './components/about/About'
-import Contact from './components/contact/Contact'
+// import About from './components/about/About'
+// import Contact from './components/contact/Contact'
 import Nav from './components/nav/Nav'
-import Experience from './components/experience/experience'
-import Services from './components/Services/Services'
-import Portfolio from './components/portofolio/Portfolio'
-import Footer from './components/footer/Footer'
-import Testimonials from './components/testimonials/Testimonials'
+// import Experience from './components/experience/experience'
+// import Services from './components/Services/Services'
+// import Portfolio from './components/portofolio/Portfolio'
+// import Footer from './components/footer/Footer'
+// import Testimonials from './components/testimonials/Testimonials'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,13 +17,13 @@ function App() {
     <>
     <Header/>
     <Nav/>
-    <About/>
-    <Experience/>
-    <Services/>
-    <Portfolio/>
-    <Testimonials/>
-    <Contact/>
-    <Footer/>
+    {/* <About/> */}
+    {/* <Experience/> */}
+    {/* <Services/> */}
+    {/* <Portfolio/> */}
+    {/* <Testimonials/> */}
+    {/* <Contact/> */}
+    {/* <Footer/> */}
     
     </>
   )
