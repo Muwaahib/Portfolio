da1edc508a932e4e0c4b44174981d0391111197c
muwaahib <muhaabdi71@gmail.com>
2023-08-04 10:19:01 +0300

    nj

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Header from './components/header/Header'
 import About from './components/about/About'
 import Contact from './components/contact/Contact'
 import Nav from './components/nav/Nav'
-// import Experience from './components/experience/experience'
+import Experience from './components/experience/experience'
 import Services from './components/Services/Services'
 import Portfolio from './components/portofolio/Portfolio'
 import Footer from './components/footer/Footer'
@@ -18,7 +18,7 @@ function App() {
     <Header/>
     <Nav/>
     <About/>
-    {/* <Experience/> */}
+    <Experience/>
     <Services/>
     <Portfolio/>
     <Testimonials/>
