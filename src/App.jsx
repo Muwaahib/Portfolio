b4cc4f484289b82e38067921372effff65d1ca93
muwaahib <muhaabdi71@gmail.com>
2023-08-04 09:19:55 +0300

    muha

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import Header from './components/header/Header'
 import About from './components/about/About'
 import Contact from './components/contact/Contact'
 import Nav from './components/nav/Nav'
-import Experience from './components/experience/experience'
-import Services from './components/Services/Services'
+// import Experience from './components/experience/experience'
+// import Services from './components/Services/Services'
 import Portfolio from './components/portofolio/Portfolio'
-import Footer from './components/footer/Footer'
+// import Footer from './components/footer/Footer'
 // import Testimonials from './components/testimonials/Testimonials'
 
 function App() {
@@ -18,12 +18,12 @@ function App() {
     <Header/>
     <Nav/>
     <About/>
-    <Experience/>
-    <Services/>
+    {/* <Experience/> */}
+    {/* <Services/> */}
     <Portfolio/>
     {/* <Testimonials/> */}
     <Contact/>
-    <Footer/>
+    {/* <Footer/> */}
     
     </>
   )
